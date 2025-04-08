398fc08ceb318176aa8076a967161dd4a18e6685
Ebenezer Quayson <quaysonebenezerawudzi@gmail.com>
2025-04-08 08:03:59 +0000

    guard scripts

--- scripts/compute_kpis.py
+++ scripts/compute_kpis.py
@@ -99,3 +99,7 @@ store_order_kpis_in_dynamodb(order_kpis)
 
 # Stop the Spark session
 spark.stop()
+
+if __name__ == "__main__":
+    # This is just for testing purposes, you can remove this part in production
+    pass
