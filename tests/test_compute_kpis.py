ee521ea52d6fda50e80a13b71c479dd1a07c30ab
Ebenezer Quayson <quaysonebenezerawudzi@gmail.com>
2025-04-08 08:54:15 +0000

    fix path error

--- tests/test_compute_kpis.py
+++ tests/test_compute_kpis.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "scripts")))
+
 import pytest
 from unittest.mock import MagicMock, patch
 import boto3
